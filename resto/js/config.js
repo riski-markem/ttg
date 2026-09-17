a1e10277867472ba2c9f2d58c8dbaff21e30d6ff
Riski <123609400+riski-markem@users.noreply.github.com>
2026-09-18 00:49:59 +0700

    Update config.js

--- resto/js/config.js
+++ resto/js/config.js
@@ -1,5 +1,5 @@
 import { app, database } from "../../shared/firebase-config.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
 import { getMessaging } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging.js";
 
@@ -14,7 +14,14 @@ try {
   console.log("FCM not supported");
 }
 
+export const authSiap = new Promise((resolve) => {
+  const berhenti = onAuthStateChanged(auth, (user) => {
+    berhenti();
+    resolve(user);
+  });
+});
+
 export const VAPID_KEY =
   "BHrjLUSNG-h2NcRw_ypWBHTr3cc-iEZ7jCA8fYzjrWO6Yh4Sqg3xTevCEAcUAbncnWwrZZfOXkuFeWMQCHnkLDs";
 export const NOMOR_ADMIN_DRIVER = "6285870422464";
-export const GOOGLE_MAPS_API_KEY = "AIzaSyBvW7x7bElI9LUZ0WTRa2yzWYefrRuK9AY"; // BUG LAMA: sebelumnya gak pernah ada di sini, padahal resto/js/geo.js butuh ini
+export const GOOGLE_MAPS_API_KEY = "AIzaSyBvW7x7bElI9LUZ0WTRa2yzWYefrRuK9AY";
